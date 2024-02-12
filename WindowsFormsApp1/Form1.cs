9397e6b0ea350cc32008bc0e5a53cfee4c4ebb9a
KakoFujimoto <ayaka20111005@yahoo.co.jp>
2024-02-12 18:57:32 +0900

    問題番号のインクリメント、デクリメントと連動して問題文が書き換わるようにした。

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -5,34 +5,49 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        //問題と解答
-        string[] quetions = { "りんご", "オレンジ", "レモン", "いちご", "きゅうり" };
-        string[] auswers = { "apple", "orange", "lemon", "strawberry", "cucumber" };
+        // 問題と解答
+        string[] questions = { "りんご", "オレンジ", "レモン", "いちご", "きゅうり" };
+        string[] answers = { "apple", "orange", "lemon", "strawberry", "cucumber" };
 
-        private int questionNumber = 1;
+        private int questionNumber = 0;
+
         public Form1()
         {
             InitializeComponent();
+            DisplayQuestion();
         }
 
+        private void DisplayQuestion()
+        {
+            if (questionNumber >= 0 && questionNumber < questions.Length)
+            {
+                Question.Text = questions[questionNumber];
+                cntNum.Text = "第" + (questionNumber + 1) + "問";
+            }
+            else
+            {
+                // インデックスが範囲外の場合、エラーを表示
+                Question.Text = "問題が見つかりません";
+                cntNum.Text = "";
+            }
+        }
 
         private void NextButton_Click(object sender, EventArgs e)
         {
-            if (questionNumber <= 4)
+            if (questionNumber < questions.Length - 1)
             {
                 questionNumber++;
-
-                cntNum.Text = "第" + questionNumber + "問";
+                DisplayQuestion();
             }
         }
+
         private void PreButton_Click(object sender, EventArgs e)
         {
-            if (questionNumber >= 2)
+            if (questionNumber > 0)
             {
                 questionNumber--;
-                cntNum.Text = "第" + questionNumber + "問";
+                DisplayQuestion();
             }
         }
-
     }
 }
